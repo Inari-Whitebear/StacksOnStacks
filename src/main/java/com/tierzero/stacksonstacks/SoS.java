0d74e832e75b20a7c2241a2fbca4d707ce9855dd
Madxmike <mep053@gmail.com>
2015-09-25 18:16:19 -0400

    Version bump - 1.0.3

--- src/main/java/com/tierzero/stacksonstacks/SoS.java
+++ src/main/java/com/tierzero/stacksonstacks/SoS.java
@@ -16,7 +16,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = SoS.MODID, name = SoS.MODID, version = SoS.VERSION, dependencies = "after:gregtech_addon;after:gregapi;")
 public class SoS {
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	public static final String MODID = "StacksOnStacks";
 	public static final String TEXTURE_BASE = MODID + ":";
 	
